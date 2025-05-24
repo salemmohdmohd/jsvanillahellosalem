032e20a89d95ce82b787ae6cdcb20c74b92eba92
salemmohdmohd <salim.farouq@gmail.com>
2025-05-24 02:34:28 +0000

    final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 import "bootstrap";
 import "./style.css";
 
+window.onload = () => {
+  const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  const action = ['ate', 'peed on', 'crushed', 'broke'];
+  const what = ['my homework', 'my phone', 'the car'];
+  const when = [
+    'before the class',
+    'when I was sleeping',
+    'while I was exercising',
+    'during my lunch',
+    'while I was praying'
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  function getRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuse = `${getRandom(who)} ${getRandom(action)} ${getRandom(what)} ${getRandom(when)}.`;
+
+  document.getElementById('excuse').innerText = excuse;
 };
